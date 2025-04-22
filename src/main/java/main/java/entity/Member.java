340a63bf458666ae72464b7a38876a895c5d00a7
devgiants75 <devgiants75@naver.com>
2025-04-22 16:35:56 +0900

    JDBC - CRUD(DAO) completed

--- src/main/java/main/java/entity/Member.java
+++ src/main/java/main/java/entity/Member.java
@@ -16,6 +16,9 @@ import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+// : JDBC 또는 ORM (DB와 JAVA를 객체화하여 연동하는 체계)에서는
+//   , 빈 객체를 먼저 만들고 setter로 값을 넣는 방식을 사용
+// >> 따라서 AllArgsConstructor도 NoArgsConstructor가 전제되어야 함! (기본 + 전체 필드 생성자)
 @Getter
 @Setter
 @ToString
